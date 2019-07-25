4fe1568558ddf82d97588ba162bb8b66b31012e6
Alan Gurgel <alangurgel1987@icloud.com>
2019-07-25 13:43:41 -0300

    Exceções de tabuleiro e programação defensiva

--- src/Tabuleiro/TabuleiroXadrez.java
+++ src/Tabuleiro/TabuleiroXadrez.java
@@ -5,6 +5,9 @@ private Integer linhas;
 private Integer colunas;
 private Peca[][] pecas;
 public TabuleiroXadrez(Integer linhas, Integer colunas) {
+	if (linhas <1 || colunas<1) {
+		throw new ExcecaoTabuleiro("Erro ao criar tabuleiro ! Deve existir linhas e colunas");
+	}
 	this.linhas = linhas;
 	this.colunas = colunas;
 	pecas = new Peca[linhas][colunas];
@@ -12,23 +15,38 @@ public TabuleiroXadrez(Integer linhas, Integer colunas) {
 public Integer getLinhas() {
 	return linhas;
 }
-public void setLinhas(Integer linhas) {
-	this.linhas = linhas;
-}
 public Integer getColunas() {
 	return colunas;
 }
-public void setColunas(Integer colunas) {
-	this.colunas = colunas;
-}
  public Peca peca(int linha, int coluna) {
+	 if (!posicaoExistente(linha, coluna)) {
+		 throw new ExcecaoTabuleiro("posição inexistente no tabuleiro.");
+	 }
 	 return pecas[linha][coluna];
  }
  public Peca peca(Posicao posicao) {
+	 if (!posicaoExistente(posicao)) {
+		 throw new ExcecaoTabuleiro("posição inexistente no tabuleiro.");
+	 }
 	 return pecas[posicao.getLinha()][posicao.getColuna()];
  }
  public void colocarPeca(Peca peca, Posicao posicao) {
+	 if(temUmaPeca(posicao)) {
+		 throw new ExcecaoTabuleiro("Existe uma peça ocupando essa posição ");
+	 }
 	 pecas[posicao.getLinha()][posicao.getColuna()]=peca;
 	 peca.posicao=posicao;
+ }
+ private boolean posicaoExistente(int linha, int coluna) {
+	 return linha >=0 && linha< linhas && coluna>=0&& coluna<colunas;
+ }
+ public boolean posicaoExistente(Posicao posicao) {
+	 return posicaoExistente(posicao.getLinha(), posicao.getColuna());
+ }
+ public boolean temUmaPeca(Posicao posicao) {
+	 if (!posicaoExistente(posicao)) {
+		 throw new ExcecaoTabuleiro("posição inexistente no tabuleiro.");
+	 }
+	 return peca(posicao)!=null;
  }
 }
